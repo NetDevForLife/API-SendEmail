43ecbf1830377b6e732fbeda6431753983adb078
Angelo Pirola <angelo9871@gmail.com>
2024-10-03 09:53:55 +0200

    Resolution issue #4, migrate project to .NET 8

--- Models/Services/Infrastructure/IEmailSenderService.cs
+++ Models/Services/Infrastructure/IEmailSenderService.cs
@@ -1,9 +1,6 @@
-using System.Threading.Tasks;
-using API_SendEmail.Models.InputModels;
-
-namespace API_SendEmail.Models.Services.Infrastructure;
+namespace Models.Services.Infrastructure;
 
 public interface IEmailSenderService
 {
-    Task SendEmailAsync(InputMailSender model);
+	Task SendEmailAsync(InputMailSender model);
 }
